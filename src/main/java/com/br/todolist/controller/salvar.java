40552d4cf781829a0ba3ad5402d49c0c6ebe6baf
Gabriel Lagrota <gabriellagrota23@gmail.com>
2024-01-16 11:20:10 -0300

    refac: - @Controller substituído por @RestController, transformando a classe em um controlador Rest, ao invés de um controlador MVC - Endpoint /salvar removido do @PostMapping e enviado para a annotation @RequestMapping, fazendo com que o contexto global dos endpoints da classe sejam contextos do /salvar

--- src/main/java/com/br/todolist/controller/salvar.java
+++ src/main/java/com/br/todolist/controller/salvar.java
@@ -3,14 +3,13 @@ package com.br.todolist.controller;
 import com.br.todolist.models.dto.TarefaDTO;
 import com.br.todolist.models.entities.TarefaEntity;
 import com.br.todolist.services.ToDoService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
+@RequestMapping("/salvar")
 public class salvar {
 
     private final ToDoService toDoService;
@@ -20,14 +19,13 @@ public class salvar {
     }
 
     // Endpoint para criar uma nova tarefa via POST
-    @PostMapping("/salvar")
+    @PostMapping
     public TarefaEntity createToDo(@RequestBody TarefaDTO tarefaDTO) {
         TarefaEntity novaTarefa = new TarefaEntity();
         novaTarefa.setMembro_nome(tarefaDTO.getMembro_nome());
         novaTarefa.setMembro_avatar(tarefaDTO.getMembro_avatar());
         novaTarefa.setTask_nome(tarefaDTO.getTask_nome());
         novaTarefa.setPrioridade(novaTarefa.getPrioridade());
-        System.out.print((novaTarefa));
         return toDoService.saveToDo(novaTarefa);
     }
 }
